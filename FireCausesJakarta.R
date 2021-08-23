81d7b537d008450f472bfb023c074260947758e7
Radian Lukman <radianlukman20@gmail.com>
2021-08-23 11:31:07 +0700

    update files

---
+++ FireCausesJakarta.R
@@ -0,0 +1,83 @@
+# Import library
+library(readr)
+library(ggplot2)
+library(dplyr)
+
+# Import datasets
+data <- read_csv("FireCausesJakarta.csv")
+head(data)
+
+# Change data type
+data$year <- as.factor(data$year)
+data$region <- as.factor(data$region)
+data$causes <- as.factor(data$causes)
+head(data)
+
+# Check missing data
+sum(is.na(data))
+
+# Make yearly aggregated data
+yearly <- aggregate(number~year, data, sum)
+ggplot(yearly, aes(x=year,y=number,group=1)) +
+  geom_line(stat='identity',color='red',size=1.5) + 
+  geom_point() +
+  geom_text(aes(label=number),hjust=1.2, color="black", size=6, parse=TRUE) +
+  ggtitle("Number of Fire Incidents In Jakarta \n 2015-2020") +
+  labs(x='Year',y='Total') +
+  theme(plot.title = element_text(hjust = 0.5,size=20,face='bold'),text = element_text(size=20))
+
+# Make trend plot each year by causes
+trend <- data %>%
+  group_by(year,causes) %>%
+  summarise(number = sum(number))
+
+ggplot(trend,aes(x = year, y = number, colour = causes, group = causes)) +
+  geom_line(size=1.2) +
+  ggtitle("Number of Fire Incidents in Jakarta \n2015-2020") +
+  labs(x='Year',y='Total') +
+  theme(plot.title = element_text(hjust = 0.5,size=15,face='bold'),text = element_text(size=15)) +
+  scale_colour_brewer(palette="Set1") 
+
+# Make Bar Chart causes of fire incidents
+causes <- aggregate(number~causes, data, sum)
+ggplot(causes, aes(x=reorder(causes,number),y=number)) +
+  geom_bar(stat="identity", fill='steelblue') +
+  geom_text(aes(label=number),hjust=1, color="black", size=6, parse=TRUE) +
+  ggtitle("Number of Fire Incidents in Jakarta \nby Cause") +
+  labs(x='Cause',y='Total') +
+  theme(plot.title = element_text(hjust = 0.5,size=15,face='bold'),text = element_text(size=15)) +
+  coord_flip()
+
+# Make Bar chart cause of fire incident by region
+region <- aggregate(number~region,data,sum)
+ggplot(region, aes(x=reorder(region,number),y=number)) +
+  geom_bar(stat="identity", fill='steelblue') +
+  geom_text(aes(label=number),hjust=1, color="black", size=6, parse=TRUE) +
+  ggtitle("Number of Fire Incidents in Jakarta \nby Region") +
+  labs(x='Region',y='Total') +
+  theme(plot.title = element_text(hjust = 0.5,size=15,face='bold'),text = element_text(size=15)) +
+  coord_flip()
+
+# Bar chart Number of fire incidents in Jakarta by Cause each year
+ggplot(data, aes(x=causes,y=number,fill=causes)) +
+  geom_bar(stat="identity") +
+  ggtitle("Number of Fire Incidents in Jakarta Based on Cause \nSeperated by Year") +
+  labs(x='Cause',y='Total') +
+  theme(plot.title = element_text(hjust=0.5,size=15,face='bold'),axis.text.x=element_text(angle = 90,vjust = 0.5, hjust=1),text = element_text(size=15)) + 
+  facet_wrap(~year)
+
+# Bar chart number of fire incidents in Jakarta by Cause each region
+ggplot(data, aes(x=causes,y=number,fill=causes)) +
+  geom_bar(stat="identity") +
+  ggtitle("Number of Fire Incidents in Jakarta Based on Cause \nSeperated by Region") +
+  labs(x='Cause',y='Total') +
+  theme(plot.title = element_text(hjust=0.5,size=15,face='bold'),axis.text.x=element_text(angle = 90,vjust = 0.5, hjust=1),text = element_text(size=15)) + 
+  facet_wrap(~region) 
+
+# Bar chart number of fire incidents in Jakarta by region each year
+ggplot(data, aes(x=region,y=number,fill=region)) +
+  geom_bar(stat="identity") +
+  ggtitle("Number of Fire Incidents in Jakarta Based on Region \nSeperated by Year") +
+  labs(x='Region',y='Total') +
+  theme(plot.title = element_text(hjust=0.5,size=15,face='bold'),axis.text.x=element_text(angle = 90,vjust = 0.5, hjust=1),text = element_text(size=15)) + 
+  facet_wrap(~year) 
--- PenyebabKebakaranJakarta.R
+++ PenyebabKebakaranJakarta.R
@@ -5,7 +5,7 @@ library(dplyr)
 
 # Import data
 data <- read_csv("PenyebabKebakaranJakarta.csv")
-head(data)
+print(data)
 
 # Mengubah jenis data
 data$tahun <- as.factor(data$tahun)
@@ -13,70 +13,70 @@ data$wilayah <- as.factor(data$wilayah)
 data$penyebab <- as.factor(data$penyebab)
 head(data)
 
+# Melihat missing data
 sum(is.na(data))
 
-# Membuat data dalam pertahhun
+# Membuat data dalam pertahun
 pertahun <- aggregate(jumlah~tahun, data, sum)
 ggplot(pertahun, aes(x=tahun,y=jumlah,group=1)) +
   geom_line(stat='identity',color='red',size=1.5) + 
   geom_point() +
   geom_text(aes(label=jumlah),hjust=1.2, color="black", size=6, parse=TRUE) +
-  ggtitle("Jumlah Kasus Kebakaran di DKI Jakarta \n 2015-2020") +
+  ggtitle("Jumlah Kejadian Kebakaran di DKI Jakarta \n 2015-2020") +
   labs(x='Tahun',y='Jumlah') +
-  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(hjust = 0.5,size=20,face='bold'),text = element_text(size=20))
 
-# Membuat trend plot tiap
+# Membuat trend plot tiap tahun
 trend <- data %>%
           group_by(tahun,penyebab) %>%
           summarise(jumlah = sum(jumlah))
 ggplot(trend,aes(x = tahun, y = jumlah, colour = penyebab, group = penyebab)) +
   geom_line(size=1.2) +
-  ggtitle("Jumlah Kasus Kebakaran di DKI Jakarta \nBerdasarkan Penyebab Tiap Tahun") +
+  ggtitle("Jumlah Kejadian Kebakaran di DKI Jakarta \n2015-2020") +
   labs(x='Tahun',y='Jumlah') +
-  theme(plot.title = element_text(hjust=0.5)) +
+  theme(plot.title = element_text(hjust = 0.5,size=15,face='bold'),text = element_text(size=15)) +
   scale_colour_brewer(palette="Set1") 
 
-
-# Membuat Bar Plot Penyebab Kebakaran
+# Membuat Bar chart Penyebab Kebakaran
 penyebab <- aggregate(jumlah~penyebab, data, sum)
 ggplot(penyebab, aes(x=reorder(penyebab,jumlah),y=jumlah)) +
   geom_bar(stat="identity", fill='steelblue') +
   geom_text(aes(label=jumlah),hjust=1, color="black", size=6, parse=TRUE) +
-  ggtitle("Jumlah Kasus Kebakaran di DKI Jakarta \nBerdasarkan Penyebab") +
+  ggtitle("Jumlah Kejadian Kebakaran di DKI Jakarta \nBerdasarkan Penyebab") +
   labs(x='Penyebab',y='Jumlah') +
-  theme(plot.title = element_text(hjust=0.5)) +
+  theme(plot.title = element_text(hjust = 0.5,size=15,face='bold'),text = element_text(size=15)) +
   coord_flip()
 
-# Membuat Bar plot Penyebab Kebakaran berdasarkan wilaya
+# Membuat Bar chart Penyebab Kebakaran berdasarkan wilayah
 wilayah <- aggregate(jumlah~wilayah,data,sum)
 ggplot(wilayah, aes(x=reorder(wilayah,jumlah),y=jumlah)) +
   geom_bar(stat="identity", fill='steelblue') +
   geom_text(aes(label=jumlah),hjust=1, color="black", size=6, parse=TRUE) +
-  ggtitle("Jumlah Kasus Kebakaran di DKI Jakarta \nBerdasarkan Kabupaten/Kota Administrasi") +
+  ggtitle("Jumlah Kejadian Kebakaran di DKI Jakarta \nBerdasarkan Kabupaten/Kota Administrasi") +
   labs(x='Kabupaten/Kota Administrasi',y='Jumlah') +
-  theme(plot.title = element_text(hjust=0.5)) +
+  theme(plot.title = element_text(hjust = 0.5,size=15,face='bold'),text = element_text(size=15)) +
   coord_flip()
 
-# Membuat bar chart jumlah kebakaran berdarkan jenisnya tiap tahun
-facet1 <- aggregate(jumlah~penyebab, data, sum)
-
+# Bar chart Jumlah Kejadian Kebakaran berdasarkan penyebab per tahun
 ggplot(data, aes(x=penyebab,y=jumlah,fill=penyebab)) +
   geom_bar(stat="identity") +
-  ggtitle("Jumlah Kasus Kebakaran di DKI Jakarta Berdasarkan Penyebabnya \nDikelompokkan per Tahun") +
+  ggtitle("Jumlah Kejadian Kebakaran di DKI Jakarta Berdasarkan Penyebabnya \nDikelompokkan per Tahun") +
   labs(x='Penyebab',y='Jumlah') +
-  theme(plot.title = element_text(hjust=0.5),axis.text.x=element_text(angle = 90,vjust = 0.5, hjust=1)) + 
-  facet_wrap(~tahun, scales='fixed')
+  theme(plot.title = element_text(hjust=0.5,size=15,face='bold'),axis.text.x=element_text(angle = 90,vjust = 0.5, hjust=1),text = element_text(size=15)) + 
+  facet_wrap(~tahun)
 
+# Bar chart Jumlah Kejadian Kebakaran berdasarkan penyebab per kabupaten/kota administrasi
 ggplot(data, aes(x=penyebab,y=jumlah,fill=penyebab)) +
   geom_bar(stat="identity") +
-  ggtitle("Jumlah Kasus Kebakaran di DKI Jakarta Berdasarkan Penyebabnya \nDikelompokkan per Kabupaten/Kota Administrasi") +
+  ggtitle("Jumlah Kejadian Kebakaran di DKI Jakarta Berdasarkan Penyebabnya \nDikelompokkan per Kabupaten/Kota Administrasi") +
   labs(x='Penyebab',y='Jumlah') +
-  theme(plot.title = element_text(hjust=0.5),axis.text.x=element_text(angle = 90,vjust = 0.5, hjust=1)) + 
+  theme(plot.title = element_text(hjust=0.5,size=15,face='bold'),axis.text.x=element_text(angle = 90,vjust = 0.5, hjust=1),text = element_text(size=15)) + 
   facet_wrap(~wilayah) 
 
+# Bar chart Jumlah Kejadian Kebakaran berdasarkan kabupaten/kota administrasi per tahun
 ggplot(data, aes(x=wilayah,y=jumlah,fill=wilayah)) +
   geom_bar(stat="identity") +
-  ggtitle("Jumlah Kasus Kebakaran di DKI Jakarta Berdasarkan Kabupaten/Kota Administrasi \nDikelompokkan per Kabupaten/Kota Administrasi") +
+  ggtitle("Jumlah Kejadian Kebakaran di DKI Jakarta \nBerdasarkan Kabupaten/Kota Administrasi \nDikelompokkan per Tahun") +
   labs(x='Kabupaten/Kota Administrasi',y='Jumlah') +
-  theme(plot.title = element_text(hjust=0.5),axis.text.x=element_text(angle = 90,vjust = 0.5, hjust=1)) + 
+  theme(plot.title = element_text(hjust=0.5,size=15,face='bold'),axis.text.x=element_text(angle = 90,vjust = 0.5, hjust=1),text = element_text(size=15)) + 
   facet_wrap(~tahun) 
